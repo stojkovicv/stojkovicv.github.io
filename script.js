04f88676223743223d55fc7512536133a6cbb077
stepski011 <vukbg.stojkovic@gmail.com>
2023-09-22 21:17:52 +0200

    Dodata ikonica, radna verzija #2

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
     if (e.target.tagName === 'LI') {
       let header = e.target.getAttribute('data-header');
       let paragraph = e.target.getAttribute('data-paragraph');
+      let githubUrl = e.target.getAttribute('data-github'); // Assume you've added a data-github attribute
 
       let column1Header = document.getElementById('column1-header');
       let column1Paragraph = document.getElementById('column1-paragraph');
+      let githubContainer = document.getElementById('github-container'); // Assuming you added this element in HTML
+
+      // Clear GitHub icon container
+      githubContainer.innerHTML = "";
 
       // Remove the existing animation class to re-trigger the animation
       column1Header.classList.remove('project-description');
@@ -16,6 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
       // Update text content
       column1Header.textContent = header;
       column1Paragraph.textContent = paragraph;
+
+      // Add GitHub icon with hyperlink
+      let githubIcon = document.createElement('a');
+      githubIcon.href = githubUrl;
+      githubIcon.target = "_blank";
+      githubIcon.innerHTML = '<i class="fab fa-github"></i>';
+      githubIcon.className = 'github-icon'; // For styling purposes
+      githubContainer.appendChild(githubIcon);
 
       // Add the animation class to trigger the fadeIn animation
       void column1Header.offsetWidth;  // Trigger reflow to restart animation
