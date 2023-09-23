3608ac89c03b6f269c23da1e55d008c4d9b3a82e
stepski011 <vukbg.stojkovic@gmail.com>
2023-09-23 10:47:23 +0200

    Radna verzija!

--- script.js
+++ script.js
@@ -42,6 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
 // JavaScript to copy email to clipboard
 document.addEventListener('DOMContentLoaded', function() {
   const copyEmailButton = document.getElementById('copy-email-button');
+  const emailAlert = document.getElementById('email-alert');
+
   copyEmailButton.addEventListener('click', function() {
     const email = "vukbg.stojkovic@gmail.com";
     const el = document.createElement('textarea');
@@ -50,6 +52,12 @@ document.addEventListener('DOMContentLoaded', function() {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
-    alert('Email address copied to clipboard');
+
+    // Show alert and hide it after 2 seconds
+    emailAlert.className = 'email-alert-visible';
+    setTimeout(() => {
+      emailAlert.className = 'email-alert-hidden';
+    }, 2000);
   });
 });
+
